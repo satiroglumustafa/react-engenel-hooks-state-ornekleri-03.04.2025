8bae01e200be9d1a4e256f37ed9e9163f9c88499
Your Name <you@example.com>
2025-04-03 18:01:31 +0300

    Fix: useEffect updated

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-
-    const [apiData, setApiData] = useState([])
-    const [loading,setLoading] = useState(true)
-
-    const apiDataFunct = async () => {
-        try {
-            const apiUrl = await fetch(url)
-            const apiJsonData = await apiUrl.json()
-            setApiData(apiJsonData.hits)
-            setLoading(false)
-        } catch (error) {
-            console.error(
-                `${window.navigator.onLine ? "İnternet bağlı ama veri çekilemedi" : "İnternete bağlı değil Bu yüzden veri çekilemedi"}`
-            )
-            setLoading(true)
-        }
-    }
-
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        apiDataFunct()
-    }, [])
+        const apiDataFunct = async () => {
+            try {
+                const apiUrl = await fetch(url);
+                const apiJsonData = await apiUrl.json();
+                setApiData(apiJsonData.hits);
+            } catch (error) {
+                console.error(
+                    `${window.navigator.onLine
+                        ? "İnternet bağlı ama veri çekilemedi"
+                        : "İnternete bağlı değil. Bu yüzden veri çekilemedi"
+                    }`
+                );
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    return {apiData,loading}
+        apiDataFunct();
+    }, [url]); // url değişirse tekrar çalışsın
 
-}
+    return { apiData, loading };
+};
 
-export default useFetch
+export default useFetch;
